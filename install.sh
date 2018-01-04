bd92eea26b7645bf199d992854f97170e988d27e
Aidan Holland <thehappydinoa@gmail.com>
2018-01-03 19:09:15 -0500

    Fixes

--- install.sh
+++ install.sh
@@ -13,14 +13,16 @@ echo "
 
 ";
 
+INSTALL_DIR="/usr/share/doc/fsociety"
+
 echo "[✔] Checking directories...";
-if [ -d "/usr/share/doc/SnapTube" ];
+if [ -d "$INSTALL_DIR" ];
 then
     echo "[◉] A directory SnapTube was found! Do you want to replace it? [Y/n]:" ;
     read mama
     if [ $mama == "y" ] ;
     then
-        rm -R "/usr/share/doc/SnapTube"
+        rm -R "$INSTALL_DIR"
     else
         exit
     fi
@@ -31,15 +33,15 @@ echo "";
 sudo apt-get install -y python-pip
 sudo pip install --upgrade youtube_dl
 sudo apt-get install -y libav-tools
-git clone https://github.com/Manisso/SnapTube.git /usr/share/doc/SnapTube;
+git clone https://github.com/Manisso/SnapTube.git $INSTALL_DIR;
 echo "#!/bin/bash
-python /usr/share/doc/SnapTube/snaptube.py" '${1+"$@"}' > snaptube;
+python $INSTALL_DIR/snaptube.py" '${1+"$@"}' > snaptube;
 chmod +x snaptube;
 sudo cp snaptube /usr/bin/;
 rm snaptube;
 
 
-if [ -d "/usr/share/doc/SnapTube" ];
+if [ -d "$INSTALL_DIR/SnapTube" ];
 then
     echo "";
     echo "[✔]Tool istalled with success![✔]";
